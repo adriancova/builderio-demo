88793fa6de10b163f962540667437651b3032cc3
Adrian Covarrubias <adriancova.95@gmail.com>
2023-12-13 20:30:15 -0800

    adding builder.io link

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,14 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          Demo app to try out builder.io
+          Demo app to try out{' '}
+          <a
+            className=' ml-2 font-bold text-blue-500'
+            href='https://builder.io'
+            target='_blank'
+          >
+            builder.io
+          </a>
         </p>
       </div>
 
@@ -13,20 +20,20 @@ export default function Home() {
       <h3 className='text-2xl font-semibold text-center mb-2'>
         All obtained from{' '}
         <a
-          className='font-bold text-3xl text-blue-500 hover:text-blue-600'
+          className='font-bold text-3xl text-blue-500 hover:text-blue-600 ml-2'
           href='https://www.freefigmatemplates.com/'
           target='_blank'
         >
           freefigmatemplates.com
         </a>
       </h3>
-      <div className='grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left'>
+      <div className='grid text-center lg:max-w-4xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left'>
         <a
           href='/real-state'
           className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
           rel='noopener noreferrer'
         >
-          <h2 className={`mb-1 text-2xl font-semibold`}>
+          <h2 className={` text-blue-500 mb-1 text-2xl font-semibold`}>
             Real state{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
               -&gt;
@@ -39,7 +46,7 @@ export default function Home() {
           className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
           rel='noopener noreferrer'
         >
-          <h2 className={`mb-1 text-2xl font-semibold`}>
+          <h2 className={` text-blue-500 mb-1 text-2xl font-semibold`}>
             Banking{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
               -&gt;
@@ -52,7 +59,7 @@ export default function Home() {
           className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
           rel='noopener noreferrer'
         >
-          <h2 className={`mb-1 text-2xl font-semibold`}>
+          <h2 className={` text-blue-500 mb-1 text-2xl font-semibold`}>
             SaaS{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
               -&gt;
